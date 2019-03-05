a0481da912d6ff33a37b0d7295431c9f45a9bf82
Robert Bedard <rbedard@plex.com>
2019-03-05 08:47:25 -0500

    Updated Config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,6 @@ module.exports = {
     sidebar: markdownFiles,
     displayAllHeaders: false,
     lastUpdated: 'Last Updated'
-  },
-  base: '/BedardFamilyFavoriteRecipes/'
+  }//,
+  //base: '/BedardFamilyFavoriteRecipes/'
 }
